a026f29529f411eee8b86006378ca8db0301c62a
Basanagouda <basanagoudad25@gmail.com>
2025-08-24 20:38:42 +0530

    Update SplashActivity.kt

--- SplashActivity.kt
+++ SplashActivity.kt
@@ -8,7 +8,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.doctorappointmentbooking.databinding.ActivitySplashBinding
 
-class SplashActivity : AppCompatActivity() {
+class SplashActivity : BaseActivity() { //it will give error so,make it open 
     private lateinit var binding: ActivitySplashBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
